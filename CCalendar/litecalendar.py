ece9b0422199121bedeff7bf1a2cf29a2b59b434
Hon-Jang Yang <hjyanghj@gmail.com>
2018-01-25 16:05:27 +0800

    modify obj_sight

--- CCalendar/litecalendar.py
+++ CCalendar/litecalendar.py
@@ -3,6 +3,7 @@ from astropy.coordinates import get_sun,FK5,PrecessedGeocentric,get_body,Angle,s
 from astropy import coordinates as coord
 from astropy import constants as const
 from astropy.coordinates import SkyCoord
+import astropy.units as u
 import math
 import numpy as np
 jieqinames=["春分","清明","穀雨","立夏","小滿","芒種","夏至","小暑","大暑","立秋",
@@ -152,8 +153,6 @@ def ShadowCone(s,rs,o,ro):
     pcenter=np.array([o[i]-dp[i]*sodir[i] for i in range(len(o))])
     #pcenter=o-np.diag(dp).dot(sodir) ##### inefficient
     return((ucenter,pcenter,uarc,parc,sodir))
-
-
 def Region(S,O,T,rt,Ucenter,Pcenter,uarc,parc,SOdir):
     res=[]
     for i in range(len(T)):
@@ -168,7 +167,7 @@ def Region(S,O,T,rt,Ucenter,Pcenter,uarc,parc,SOdir):
             continue
         vu=T[i]-Ucenter[i]
         dist=np.linalg.norm(vu)
-        if vu.dot(SOdir[i])<=0:  # 
+        if vu.dot(SOdir[i])<=0:  # T 在 U 的右側
             if dist<=rt:         # 本影頂點在球T內部
                 res.append('F')  # 球T與本影區有重疊
                 continue
@@ -179,9 +178,9 @@ def Region(S,O,T,rt,Ucenter,Pcenter,uarc,parc,SOdir):
             elif usep<= uarc[i]+utarc: # alpha-gamma<TUO<=alpha+gamma
                 res.append('F')  # 球T部分落在本影區
             else:               # alpha+gamma < TUO
-                res.append('P')  # 球T落在半影區
+                res.append('P')  # 球T與半影區有重疊
             continue
-        else:                    # 
+        else:                    # T 在 U 的左側
             if dist<=rt:         # 本影頂點在球T內部
                 res.append('F')  # 球T與本影區有重疊
                 continue
@@ -192,10 +191,9 @@ def Region(S,O,T,rt,Ucenter,Pcenter,uarc,parc,SOdir):
             elif usep<= uarc[i]+utarc: # alpha-gamma<pi-TUO < alpha+gamma, pi-alpha-gamma< TUO <pi-alpha+gamma
                 res.append('C')  # 球T局部落在偽本影
             else:                # pi-TUO>alpha+gamma, TUO<pi-alpha-gamma
-                res.append('P')  # 球T全部落在半影區
+                res.append('P')  # 球T與半影區有重疊
             continue
     return(res)  
-
 def EarthRegion(times):
     gmttimes=times-eighthours
     sun=get_body('sun', gmttimes,  ephemeris='jpl')
@@ -211,8 +209,6 @@ def EarthRegion(times):
     earthpos=np.zeros((len(sunpos),3))
     Ucenter,Pcenter,uarc,parc,SOdir=ShadowCone(sunpos,rs,moonpos,ro)
     return(Region(sunpos,moonpos,earthpos,re,Ucenter,Pcenter,uarc,parc,SOdir))
-    
-
 def SolarEclipseOne0(start,end):
     #print((Time(end)-Time(start)).value)
     minutecount=int(np.ceil((Time(end)-Time(start)).value*1440))
@@ -276,7 +272,6 @@ def LunarRegion(times,air):
     # 此處採用Danjon 的方法
     Ucenter,Pcenter,uarc,parc,SOdir=ShadowCone(sunpos,rs,earthpos,(1+air)*re)
     return(Region(sunpos,earthpos,moonpos,rm,Ucenter,Pcenter,uarc,parc,SOdir))
-
 def LunarEclipseOne(start,end,air=0.02):
     minutecount=int(np.ceil((Time(end)-Time(start)).value*1440))
     times=Time(start)+np.linspace(0.,1.,minutecount+1)*(Time(end)-Time(start))
@@ -304,7 +299,6 @@ def LunarEclipse(start,end,air=0.02):
         end=dark+halfday
         #print([str(start),str(end)])
         LunarEclipseOne(str(start),str(end))
-
 def Eclipse(start,end,air=0.02):
     darkmoon=CCTerm(start,end,'moon')
     # darkmoon=[codedate[1] for codedate in darkmoon if codedate[0]==0 ]
@@ -320,8 +314,7 @@ def Eclipse(start,end,air=0.02):
             SolarEclipseOne(str(start),str(end))
         else:
             LunarEclipseOne(str(start),str(end),air)
-
-def obj_sight(date,objname,diskanglestr,lonstr,latstr,tzinhours):
+def obj_sight0(date,objname,diskanglestr,lonstr,latstr,tzinhours):
     """
     date: string, date of observing. 
     objname : string, name of sun, moon or planet in solar system.
@@ -341,13 +334,13 @@ def obj_sight(date,objname,diskanglestr,lonstr,latstr,tzinhours):
     with solar_system_ephemeris.set('jpl'):
         n=len(times)
         gmttimes=times-deltat   # 世界時間 UTC
-        # 標的物在及時赤道座標系統座標
+        # 標的物在真赤道座標系統座標
         bodies=get_body(objname,gmttimes).transform_to(PrecessedGeocentric(equinox=gmttimes))  
         sides=gmttimes.sidereal_time('apparent', 'greenwich')+lon    # local sidereal time
         x=bodies.cartesian.x.to('m').value        # x,y,z為標的物在及時赤道座標系統的直角座標   
         y=bodies.cartesian.y.to('m').value
         z=bodies.cartesian.z.to('m').value
-        px=np.array(a*np.cos(lat)*np.cos(sides))  # px,py,pz 為觀測地在及時赤道座標系統的直角坐標
+        px=np.array(a*np.cos(lat)*np.cos(sides))  # px,py,pz 為觀測地在真赤道座標系統的直角坐標
         py=np.array(a*np.cos(lat)*np.sin(sides))
         pz=np.array(a*np.sin(lat)*np.ones(n))
     positions=np.vstack((px,py,pz)).T  # n x 3 array
@@ -391,6 +384,87 @@ def obj_sight(date,objname,diskanglestr,lonstr,latstr,tzinhours):
                 ratio=(0-left)/(az[i+1]-az[i])
                 t=times[i]+(times[i+1]-times[i])*ratio
                 print("Transit",t,"Alt",alt[i]+(alt[i+1]-alt[i])*ratio,"Az","North")
+def obj_sight(date,objname,lonstr,latstr,tzinhours,approx=True):
+    """
+    date: string, date of observing. 
+    objname : string, name of sun, moon or planet in solar system.
+    lonstr : string, longitute of observing location on earth.
+    latstr : string, latitute of observing location on earth.
+    ltzinhours : float, time zone in hours of observing location.
+    approx : bool, True for delta_ut1_utc=0
+    return : print out rise time, set time, transit time and cooresponding alt/az.
+    """
+    lon=Angle(lonstr)
+    lat=Angle(latstr)
+    if objname=='sun':
+      r_obj=const.R_sun.to('m').value
+    else:
+      if objname=="moon":
+        r_obj=1737000
+    #halfdisk=Angle(diskanglestr)*0.5
+    date=Time(Time(date,out_subfmt='date').iso)
+    times=date+np.linspace(0.,1440.,1441)*TimeDelta(60,format='sec')  # 每分鐘計算位置
+    deltat=tzinhours*TimeDelta(3600,format='sec')
+    a=const.R_earth.to('m').value       # 地球半徑
+    with solar_system_ephemeris.set('jpl'):
+        n=len(times)
+        gmttimes=times-deltat   # 世界時間 UTC
+        # 標的物在真赤道座標系統座標
+        bodies=get_body(objname,gmttimes).transform_to(PrecessedGeocentric(equinox=gmttimes)) 
+        if approx:
+          gmttimes.delta_ut1_utc = 0. 
+        sides=gmttimes.sidereal_time('apparent', 'greenwich')+lon    # local sidereal time
+        x=bodies.cartesian.x.to('m').value        # x,y,z為標的物在及時赤道座標系統的直角座標   
+        y=bodies.cartesian.y.to('m').value
+        z=bodies.cartesian.z.to('m').value
+        px=np.array(a*np.cos(lat)*np.cos(sides))  # px,py,pz 為觀測地在真赤道座標系統的直角坐標
+        py=np.array(a*np.cos(lat)*np.sin(sides))
+        pz=np.array(a*np.sin(lat)*np.ones(n))
+    positions=np.vstack((px,py,pz)).T  # n x 3 array
+    objs=np.vstack((x,y,z)).T          # n x 3 array
+    # e_z 的每一行為天頂方向單位向量，e_n的每一行為地表往北單位向量，e_e的每一行為地表往東單位向量
+    e_z=np.vstack((np.cos(lat)*np.cos(sides),np.cos(lat)*np.sin(sides),np.sin(lat)*np.ones(n)))  # 3 x n
+    e_n=np.vstack((-np.sin(lat)*np.cos(sides),-np.sin(lat)*np.sin(sides),np.cos(lat)*np.ones(n))) # 3 x n
+    e_e=np.vstack((-np.sin(sides),np.cos(sides),np.zeros(n)))                 # 3 x n
+    coords=np.zeros((n,3))   # coords 的每一列代表標的物在地表座標系統的直角坐標
+    az=np.zeros(n)           # az 標的物的方位角，北方為0，往東為正，正東方90度，正南方180度，正西方270度。
+    alt=np.zeros(n)          # alt 標的物的仰角
+    for i in range(n):
+        coords[i]=np.array(np.vstack((e_e[:,i],e_n[:,i],e_z[:,i]))).dot(objs[i]-positions[i])
+        r=np.sqrt(coords[i][0]**2+coords[i][1]**2)
+        rho=np.sqrt(coords[i][0]**2+coords[i][1]**2+coords[i][2]**2)
+        alt[i]=np.arcsin(coords[i][2]/rho)*180/np.pi
+        az[i]=np.arctan2(coords[i][0],coords[i][1])*180/np.pi
+        if az[i]<0:
+            az[i]=az[i]+360
+    if objname=="sun" or objname=="moon" :
+      halfdisk=np.arcsin(r_obj/rho)*180/np.pi*u.deg
+    else :
+      halfdisk=0*u.deg
+    thresh=-(halfdisk+Angle("0d34m0s")).deg   # 大氣折射，讓光線轉彎了34角分
+    altdiff=alt-thresh        # altdif 變號的時候就是標的物升起或沉沒的區間，用內插進行估計
+    for i in range(len(times)-1):
+        if altdiff[i]*altdiff[i+1] <=0 :
+            t=times[i]+(times[i+1]-times[i])*(0-altdiff[i])/(altdiff[i+1]-altdiff[i])
+            if altdiff[i]<0:
+                print("出",t,"方位",az[i])
+            else:
+                print("沒",t,"方位",az[i])
+        if not (az[i] // 180 == az[i+1] // 180) and alt[i]>0:
+            if np.abs(az[i]-180)<30:
+                ratio=(180-az[i])/(az[i+1]-az[i])
+                t=times[i]+(times[i+1]-times[i])*ratio
+                print("中天",t,"仰角",alt[i]+(alt[i+1]-alt[i])*ratio,"方位","南")
+            else:
+                left=az[i]
+                right=az[i+1]
+                if left>300:
+                    left=left-360
+                if right>300:
+                    right=right-360
+                ratio=(0-left)/(az[i+1]-az[i])
+                t=times[i]+(times[i+1]-times[i])*ratio
+                print("中天",t,"仰角",alt[i]+(alt[i+1]-alt[i])*ratio,"方位","南")
 # 台北(東經121.5654度，北緯25.032969度，時差+8小時)，標的物太陽(sun)的視角32角分。
 # Ex.1
 # 用下面指令列印出2018年1月1日在台北的日出、日落、中天時刻以及方位與仰角。
