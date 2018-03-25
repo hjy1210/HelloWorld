ca55f3d7a1173df9ad542e157a7b12113639d14e
unknown <hjyang@gmail.com>
2018-03-26 07:59:54 +0800

    add StringNormalize

---
+++ StringNormalize/CJKUtil/Normalize.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CJKUtil
+{
+    public static class Normalize
+    {
+		public static Encoding unicode = new UnicodeEncoding();
+		static string[] U3400 = { "3400", "4DB5" };
+		static string[] U4E00 = { "4E00", "9FEA" };
+		static string[] UF900 = { "F900", "FAD9" };
+		static string[] code ={"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+			"A", "B", "C", "D", "E", "F"};
+		static Dictionary<string, int> Hex = new Dictionary<string, int>{
+			{ "0", 0 },{ "1", 1 },{ "2", 2 },{ "3", 3 },{ "4", 4 },{ "5", 5 },{ "6", 6 },{ "7", 7 },
+			{ "8", 8 },{ "9", 9 },{ "A", 10 },{ "B", 11 },{ "C", 12 },{ "D", 13 },{ "E", 14 },{ "F", 15 }
+		};
+		static Encoding big5 = Encoding.GetEncoding("big5");
+		static Encoding utf8 = Encoding.GetEncoding("utf-8");
+
+		public static bool IsBig5(string s)
+		{
+			return s == Convert(s);
+		}
+		public static string ToBig5ToUnicode(string s)
+		{
+			Encoding big5 = Encoding.GetEncoding("big5");
+			byte[] big5Bytes = big5.GetBytes(s);
+			//int n = big5Bytes.Length;
+			char[] big5Chars = new char[big5.GetCharCount(big5Bytes, 0, big5Bytes.Length)];
+			big5.GetChars(big5Bytes, 0, big5Bytes.Length, big5Chars, 0);
+
+			string data=new String(big5Chars);
+			/*data = data + n.ToString() + ":";
+			for (int i = 0; i < big5Bytes.Length; i++)
+			{
+				data = data + code[big5Bytes[i] / 16] + code[big5Bytes[i] % 16];
+			}*/
+			return data;
+		}
+		public static string Big5String(byte ubyte,byte lbyte)
+		{
+			byte[] bytes = new byte[2];
+			bytes[0] = ubyte;
+			bytes[1] = lbyte;
+			Encoding big5 = Encoding.GetEncoding("big5");
+			Char[] big5Chars = big5.GetChars(bytes);
+			string s = new string(big5Chars);
+			return s;
+		}
+		public static string Convert(string encodingFrom,string encodingTo,string data)
+		{
+			Encoding from = Encoding.GetEncoding(encodingFrom);
+			Encoding to = Encoding.GetEncoding(encodingTo);
+			byte[] fromBytes = from.GetBytes(data);
+			byte[] toBytes = Encoding.Convert(from, to, fromBytes);
+			char[] toChars = new Char[to.GetCharCount(toBytes, 0, toBytes.Length)];
+			to.GetChars(toBytes, 0, toBytes.Length, toChars,0);
+			return new string(toChars);
+		}
+		static string Convert(string big5String)
+		{
+
+			// convert string to bytes
+			byte[] big5Bytes = big5.GetBytes(big5String);
+
+			// convert encoding from big5 to utf8
+			byte[] utf8Bytes = Encoding.Convert(big5, utf8, big5Bytes);
+
+			char[] utf8Chars = new char[utf8.GetCharCount(utf8Bytes, 0, utf8Bytes.Length)];
+			utf8.GetChars(utf8Bytes, 0, utf8Bytes.Length, utf8Chars, 0);
+
+			return new string(utf8Chars);
+		}
+		public static void CJKNCNBig5(string s)
+		{
+			UnicodeEncoding unicode = new UnicodeEncoding();
+
+			// Create a string that contains Unicode characters.
+			//String unicodeString =
+			//	"This Unicode string contains two characters " +
+			//	"with codes outside the traditional ASCII code range, " +
+			//	"Pi (\u03a0) and Sigma (\u03a3).";
+			Console.WriteLine("Original string:");
+			Console.WriteLine(s);
+
+			// Encode the string.
+			Byte[] encodedBytes = unicode.GetBytes(s);
+			Console.WriteLine();
+			Console.WriteLine("Encoded bytes:");
+			foreach (Byte b in encodedBytes)
+			{
+				Console.Write("[{0:X2}]", b);
+			}
+			Console.WriteLine();
+
+		}
+		static ushort FourChar2UShort(string s)
+		{
+			ushort n = (ushort)((Hex[s.Substring(0, 1)] * 16 + Hex[s.Substring(1, 1)]) * 256 +
+				Hex[s.Substring(2, 1)] * 16 + Hex[s.Substring(3, 1)]);
+			return n;
+		}
+		public static string UnicodeRange(string start, string end)
+		{
+			ushort ustart = FourChar2UShort(start);
+			ushort uend = FourChar2UShort(end);
+			StringBuilder str = new StringBuilder();
+			for (ushort n= ustart; n <= uend; n = (ushort)(n + 1))
+			{
+				str.Append(ushort2string(n));
+			}
+			return str.ToString();
+		}
+		public static string ushortCode(ushort n)
+		{
+			int un = n / 256;
+			int ln = n % 256;
+			return code[un / 16] + code[un % 16] + code[ln / 16] + code[ln % 16];
+		}
+		public static string ushort2string(ushort n)
+		{
+			byte[] buffer = new byte[2];
+			buffer[0] =(byte)( n % 256);
+			buffer[1] = (byte)(n / 256);
+			return unicode.GetString(buffer);
+		}
+		public static string CJKNormalizeNotInBig5(string start,string end)
+		{
+			ushort ustart = FourChar2UShort(start);
+			ushort uend = FourChar2UShort(end);
+			StringBuilder sb = new StringBuilder();
+			int count = 0;
+			int normalorig=0;
+			int normal = 0;
+			int unnormal = 0;
+			byte[] unicodeBytes;
+			for (ushort n = ustart; n <= uend; n = (ushort)(n + 1))
+			{
+				string s = ushort2string(n);
+				string snormal = s.Normalize();
+				if (IsBig5(snormal))
+				{
+					normal++;
+					if (s != snormal)
+					{
+						sb.Append(ushortCode(n) + " ");
+						sb.Append(s + " ");
+						unicodeBytes = unicode.GetBytes(snormal);
+						sb.Append(code[unicodeBytes[1] / 16] + code[unicodeBytes[1] % 16] + code[unicodeBytes[0] / 16] + code[unicodeBytes[0] % 16]);
+						sb.Append(" " + snormal + " ");
+						//sb.Append(ToBig5ToUnicode(snormal));
+						count++;
+						sb.AppendLine("");
+					}
+					else
+					{
+						normalorig++;
+					}
+				}
+				else
+				{
+					unnormal++;
+					sb.Append(ushortCode(n) + " ");
+					sb.Append(s + " ");
+					unicodeBytes = unicode.GetBytes(snormal);
+					sb.Append(code[unicodeBytes[1] / 16] + code[unicodeBytes[1] % 16] + code[unicodeBytes[0] / 16] + code[unicodeBytes[0] % 16]);
+					sb.AppendLine(" " + snormal + " ###");
+				}
+			}
+			sb.AppendLine(string.Format("{0} characters,{1} characters is with normalized version in Big5, {2} characters is with normalized version outside Big5", uend - ustart + 1, normal,unnormal));
+			sb.AppendLine(string.Format("{0} characters is normalized and in Big 5, {1} characters with normalized version in Big 5", normalorig, count));
+			return sb.ToString();
+		}
+		public static void CJKNormalizeNotInBig5(string startend)
+		{
+			StreamWriter sw = new StreamWriter(startend,false,Encoding.UTF8);
+			sw.WriteLine(CJKNormalizeNotInBig5(startend.Substring(0, 4), startend.Substring(4, 4)));
+			sw.Flush();
+			sw.Close();
+		}
+	}
+}
---
+++ StringNormalize/StringNormalize/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using CJKUtil;
+
+namespace StringNormalize
+{
+	class Program
+	{
+		static void test0()
+		{
+			string s1 = new String(new char[] { '\u0063', '\u0301', '\u0327', '\u00BE' });
+			string s2 = null;
+			string divider = new String('-', 80);
+			divider = String.Concat(Environment.NewLine, divider, Environment.NewLine);
+
+			Show("s1", s1);
+			Console.WriteLine();
+			Console.WriteLine("U+0063 = LATIN SMALL LETTER C");
+			Console.WriteLine("U+0301 = COMBINING ACUTE ACCENT");
+			Console.WriteLine("U+0327 = COMBINING CEDILLA");
+			Console.WriteLine("U+00BE = VULGAR FRACTION THREE QUARTERS");
+			Console.WriteLine(divider);
+
+			Console.WriteLine("A1) Is s1 normalized to the default form (Form C)?: {0}",
+										 s1.IsNormalized());
+			Console.WriteLine("A2) Is s1 normalized to Form C?:  {0}",
+										 s1.IsNormalized(NormalizationForm.FormC));
+			Console.WriteLine("A3) Is s1 normalized to Form D?:  {0}",
+										 s1.IsNormalized(NormalizationForm.FormD));
+			Console.WriteLine("A4) Is s1 normalized to Form KC?: {0}",
+										 s1.IsNormalized(NormalizationForm.FormKC));
+			Console.WriteLine("A5) Is s1 normalized to Form KD?: {0}",
+										 s1.IsNormalized(NormalizationForm.FormKD));
+
+			Console.WriteLine(divider);
+
+			Console.WriteLine("Set string s2 to each normalized form of string s1.");
+			Console.WriteLine();
+			Console.WriteLine("U+1E09 = LATIN SMALL LETTER C WITH CEDILLA AND ACUTE");
+			Console.WriteLine("U+0033 = DIGIT THREE");
+			Console.WriteLine("U+2044 = FRACTION SLASH");
+			Console.WriteLine("U+0034 = DIGIT FOUR");
+			Console.WriteLine(divider);
+
+			s2 = s1.Normalize();
+			Console.Write("B1) Is s2 normalized to the default form (Form C)?: ");
+			Console.WriteLine(s2.IsNormalized());
+			Show("s2", s2);
+			Console.WriteLine();
+
+			s2 = s1.Normalize(NormalizationForm.FormC);
+			Console.Write("B2) Is s2 normalized to Form C?: ");
+			Console.WriteLine(s2.IsNormalized(NormalizationForm.FormC));
+			Show("s2", s2);
+			Console.WriteLine();
+
+			s2 = s1.Normalize(NormalizationForm.FormD);
+			Console.Write("B3) Is s2 normalized to Form D?: ");
+			Console.WriteLine(s2.IsNormalized(NormalizationForm.FormD));
+			Show("s2", s2);
+			Console.WriteLine();
+
+			s2 = s1.Normalize(NormalizationForm.FormKC);
+			Console.Write("B4) Is s2 normalized to Form KC?: ");
+			Console.WriteLine(s2.IsNormalized(NormalizationForm.FormKC));
+			Show("s2", s2);
+			Console.WriteLine();
+
+			s2 = s1.Normalize(NormalizationForm.FormKD);
+			Console.Write("B5) Is s2 normalized to Form KD?: ");
+			Console.WriteLine(s2.IsNormalized(NormalizationForm.FormKD));
+			Show("s2", s2);
+			Console.WriteLine();
+
+		}
+		static void test4()
+		{
+			string str = "凉";
+			byte[] big5bytes = System.Text.Encoding.GetEncoding("BIG5").GetBytes(str);
+			Console.WriteLine(big5bytes.Length); // output: 1
+			string str2 = "\u51C9";
+			byte[] big5bytes2 = System.Text.Encoding.GetEncoding("BIG5").GetBytes(str2);
+			Console.WriteLine(big5bytes2.Length); // output: 1
+		}
+		static void test3()
+		{
+			string a = "㐀";
+			string b = Normalize.Convert("utf-8", "big5", a);
+			string data = String.Format("Source={0},Target={1},Is equal?{2}", a, b, a == b);
+			System.IO.StreamWriter sw = new System.IO.StreamWriter("test.txt", false, Encoding.UTF8);
+			string a2 = a.Normalize();
+			string b2 = Normalize.Convert("utf-8", "big5", a2);
+			sw.WriteLine(data);
+			sw.Close();
+
+		}
+		static void test5()
+		{
+			Normalize.CJKNormalizeNotInBig5("4E009FEA");
+			Normalize.CJKNormalizeNotInBig5("F900FAD9");
+			Normalize.CJKNormalizeNotInBig5("34004DB5");
+		}
+		static void test6()
+		{
+			byte[] unicodeBytes = { 0,78,1,78,2,78,3,78};
+			Encoding unicode = new UnicodeEncoding();
+			Encoding big5 = Encoding.GetEncoding("big5");
+			char[] unicodeChars =unicode.GetChars(unicodeBytes);
+			string s=new String(unicodeChars);
+			StreamWriter sw = new StreamWriter("test.txt", false, Encoding.GetEncoding("utf-8"));
+			sw.WriteLine(s);
+			sw.Close();
+		}
+		static void test7()
+		{
+			//byte[] unicodeBytes = {0, 52 };
+			byte[] unicodeBytes = { 0, 78 };  // 0,78 = 0x4E00 --> A440=一
+			Encoding unicode = new UnicodeEncoding();
+			char[] unicodeChars = unicode.GetChars(unicodeBytes);
+			string s = new String(unicodeChars);
+			Console.WriteLine(s);
+			Console.WriteLine(Normalize.IsBig5(s));
+		}
+		static void test8()
+		{
+			string s = Normalize.Big5String(164,65); // 一 A440->0x4E00 乙 A441=164,65 -> 0x4E59
+			Console.WriteLine("U+{0:X4}", (int)s[0]);
+			Console.WriteLine(s);
+		}
+		static void Main()
+		{
+			test3();
+			//test5();
+			//test6();
+			//test7();
+			//test8();
+		}
+		static void test2()
+		{
+			test("机機");
+			test("數數");
+			test("啟啓啓啟");
+			test("兀兀");
+			test("嗀嗀");
+			test("黄黃");
+			Console.WriteLine("{0} 是 Big5? {1}", "數", Normalize.IsBig5("數"));
+			Console.WriteLine("{0} 是 Big5? {1}", "數", Normalize.IsBig5("數"));
+			Normalize.CJKNCNBig5("數數");
+			Console.WriteLine(Normalize.ushort2string((15*16+9)*256+(6*16+9)));
+			Console.WriteLine(Normalize.UnicodeRange("F969","F973"));
+			Normalize.CJKNormalizeNotInBig5("F900FAD9");
+			Console.WriteLine("Press any key to continue");
+			Console.ReadKey();
+		}
+		static void test(string s1)
+		{
+			// string s1 = "數數气氣"; //new String(new char[] { '\u0063', '\u0301', '\u0327', '\u00BE' });
+			string s2 = s1.Normalize();
+			string s3 = s1.Normalize(NormalizationForm.FormKC);
+			string divider = new String('-', 80);
+			divider = String.Concat(Environment.NewLine, divider, Environment.NewLine);
+
+			Show(s1, s1);
+			Show(s2, s2);
+			Show(s3, s3);
+			Console.WriteLine("s1) Is s1 normalized to the default form (Form C)?: {0}",
+							 s1.IsNormalized(NormalizationForm.FormC));
+			Console.WriteLine("s1) Is s1 normalized to the default form (Form KC)?: {0}",
+							 s1.IsNormalized(NormalizationForm.FormKC));
+			Console.WriteLine("s2) Is s2 normalized to the default form (Form C)?: {0}",
+							 s2.IsNormalized(NormalizationForm.FormC));
+			Console.WriteLine("s2) Is s2 normalized to the default form (Form KC)?: {0}",
+							 s2.IsNormalized(NormalizationForm.FormKC));
+			Console.WriteLine("s3) Is s3 normalized to the default form (Form C)?: {0}",
+							 s3.IsNormalized(NormalizationForm.FormC));
+			Console.WriteLine("s3) Is s3 normalized to the default form (Form KC)?: {0}",
+							 s3.IsNormalized(NormalizationForm.FormKC));
+
+			Console.WriteLine();
+
+		}
+		private static void Show(string title, string s)
+		{
+			Console.Write("Characters in string {0} = ", title);
+			foreach (short x in s)
+			{
+				Console.Write("{0:X4} ", x);
+			}
+			Console.WriteLine();
+		}
+
+	}
+}
